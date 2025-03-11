439a24841ae2e5628604f1af83edf0a984f0165e
Othmane Abderrazik <othmane232004@gmail.com>
2025-03-11 12:09:41 +0000

    fixing package.json & adding fontAwesome

---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,14 @@
+import { IconDefinition} from '@fortawesome/angular-fontawesome';
+import { faCartShopping, faTruckFast, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faTwitter, faXTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faUser,
+  faCartShopping,
+  faTruckFast,
+  faTwitter,
+  faFacebook,
+  faYoutube,
+  faInstagram,
+  faXTwitter,
+  ];
